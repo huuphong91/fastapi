fa4832bac44fffafa20018b09065a59d8a0e1360
Huu Phong <huuphong91@gmail.com>
2024-11-30 04:02:40 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request
+import models
+from database import engine
+from routers import auth, todos, admin, users
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/healthy")
+def health_check():
+    return {'status': 'Healthy'}
+
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
